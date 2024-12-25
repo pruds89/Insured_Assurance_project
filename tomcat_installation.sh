3ac1cecfb73499dded806071b3b06378383f3879
Prudhvi Bachineni <prudhvi.ece@gmail.com>
2024-12-25 11:28:44 +0530

    reverting to the successful case

--- tomcat_installation.sh
+++ tomcat_installation.sh
@@ -5,7 +5,7 @@ apt install -y tomcat9 tomcat9-admin
 #sleep 15
 sed -i 's/8080/9090/g' /etc/tomcat9/server.xml
 sed -i 's/for java_version in 11 10 9 8/for java_version in 21 11 10 9 8/' /usr/libexec/tomcat9/tomcat-locate-java.sh
-#systemctl restart tomcat9 
+systemctl restart tomcat9 
 #sleep 10 
 
 

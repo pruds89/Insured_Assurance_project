392947e9b1f2b00f4a6576f8fab1865a38877688
Prudhvi Bachineni <prudhvi.ece@gmail.com>
2024-12-25 11:27:00 +0530

    Making some test cases to fail the pipeline

--- tomcat_installation.sh
+++ tomcat_installation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 apt update 
-sleep 10 
+#sleep 10 
 apt install -y tomcat9 tomcat9-admin
-sleep 15
+#sleep 15
 sed -i 's/8080/9090/g' /etc/tomcat9/server.xml
 sed -i 's/for java_version in 11 10 9 8/for java_version in 21 11 10 9 8/' /usr/libexec/tomcat9/tomcat-locate-java.sh
-systemctl restart tomcat9 
-sleep 10 
+#systemctl restart tomcat9 
+#sleep 10 
 
 
